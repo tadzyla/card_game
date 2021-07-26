7a2af0779e7321a00a18e63f53a2eb4344811dab
tadzyla <tadas.zvinklys@gmail.com>
2021-07-26 14:23:43 +0100

    adding card functions adding div's and styling

--- deck.js
+++ deck.js
@@ -1,3 +1,5 @@
+import { format } from "node:path"
+
 const SUITS = ["♠", "♣", "♥", "♦"]
 const VALUES = [
     "A",
@@ -15,9 +17,22 @@ const VALUES = [
     "K"
   ]
 
-class Deck {
-    constructor(cards) {
+export default class Deck {
+    constructor(cards = freshDeck()) {
         this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        for(let i = this.numberOfCards - 1; i > 0; i-- ) {
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.card[i] = oldValue
+        }
     }
 }
 
@@ -26,4 +41,12 @@ class Card {
         this.suit = suit
         this.value = value
     }
+}
+
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
 }
---
+++ script.js
@@ -0,0 +1,7 @@
+import Deck from './deck.js';
+
+const deck = new Deck()
+
+deck.shuffle()
+
+console.log(deck.cards)
